4651903f3e4fb06362f21f9ee103f5d74d36c64c
Shay Zigdon <zigshay@gmail.com>
2024-01-08 18:14:03 +0200

    build

---
+++ public/assets/vidstack-8sfc5owa-34fa760e.js
@@ -0,0 +1,1 @@
+import{H as t}from"./vidstack-HAbxB-FO-da590638.js";import"./index-cc9dba25.js";import"./vidstack-UK1DzmJ--83b7ea08.js";class u extends t{constructor(){super(...arguments),this.$$PROVIDER_TYPE="AUDIO"}get type(){return"audio"}setup(e){super.setup(e),this.type==="audio"&&e.delegate.Pb("provider-setup",this)}get audio(){return this.Ga}}export{u as AudioProvider};
